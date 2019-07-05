43f9dc5c03aaab0a8b1a9aa97ac915648cbed695
Ruben Buelvas <rcbuelvasv@gmail.com>
2019-07-05 14:35:14 -0500

    Forms

--- index.js
+++ index.js
@@ -5,18 +5,32 @@ $('a[href^="#"]').click(function () {
     return;
 });
 
-var data = { email: "" };
+var data = {subject: "", 
+			name: "", 
+			lastname: "",
+			company: "",
+			country: "",
+			phone: "",
+			email: "", 
+			message: ""};
+
 var button = document.getElementById("submit");
 
-
 button.addEventListener("click", function(event) {
-    data = { email: String(document.getElementById("emailInput").value) };
-	//console.log(data);
+    data = {subject: String(document.getElementById("subject").value),
+    		name: String(document.getElementById("name").value), 
+			lastname: String(document.getElementById("lastname").value),
+			company: String(document.getElementById("company").value),
+			country: String(document.getElementById("country").value),
+			phone: String(document.getElementById("phone").value),
+			email: String(document.getElementById("email").value), 
+			message: String(document.getElementById("message").value)};
+	console.log(data);
 
-    fetch("http://atarraya.eastus.cloudapp.azure.com/api/submitEmail", {
+    fetch("http://someurl.com", {
         method: "POST",
         body: JSON.stringify(data)
     }).then(res => {
-        alert(String(data.email) + "ok")
+        alert("Gracias por ponerse en contacto con nosotros, " + String(data.name) + ".")
     });
 });
